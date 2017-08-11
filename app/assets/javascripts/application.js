f062766933e290a5e68fa6a22e239505b5e4c180
Garrett <Garrett@Mac-10ddb1cebf4f.local>
2017-08-10 20:49:52 -0700

    initial upload/edits

---
+++ app/assets/javascripts/application.js
@@ -0,0 +1,39 @@
+// This is a manifest file that'll be compiled into application.js, which will include all the files
+// listed below.
+//
+// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, or any plugin's
+// vendor/assets/javascripts directory can be referenced here using a relative path.
+//
+// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
+// compiled file. JavaScript code in this file should be added after the last require_* statement.
+//
+// Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
+// about supported directives.
+//
+
+//= require rails-ujs
+//= require turbolinks
+//= require jquery
+//= require_tree .
+window.smoothScroll = function(target) {
+    var scrollContainer = target;
+    do { //find scroll container
+        scrollContainer = scrollContainer.parentNode;
+        if (!scrollContainer) return;
+        scrollContainer.scrollTop += 1;
+    } while (scrollContainer.scrollTop == 0);
+
+    var targetY = 0;
+    do { //find the top of target relatively to the container
+        if (target == scrollContainer) break;
+        targetY += target.offsetTop;
+    } while (target = target.offsetParent);
+
+    scroll = function(c, a, b, i) {
+        i++; if (i > 30) return;
+        c.scrollTop = a + (b - a) / 30 * i;
+        setTimeout(function(){ scroll(c, a, b, i); }, 20);
+    }
+    // start scrolling
+    scroll(scrollContainer, scrollContainer.scrollTop, targetY, 0);
+}
---
+++ app/controllers/resume_controller.rb
@@ -0,0 +1,7 @@
+class ResumeController < ApplicationController
+  def index
+    # pdf_filename = File.join(Rails.root, "assets/Garrett-Hughes-Resume.pdf")
+    # send_file(pdf_filename, :filename => "Garrett-Hughes-Resume.pdf", :disposition => 'inline', :type => "resume/index")
+  end
+
+end
